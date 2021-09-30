4c5a0a489caa4bde41edaff308203b63f10716ba
hammed103 <hammedfree@gmail.com>
2021-09-30 05:28:59 +0100

    os

--- user.py
+++ user.py
@@ -6,6 +6,12 @@ import random
 from credentials import *
 import schedule
 
+from os import environ
+consumer_key = environ['consumer_key']
+consumer_secret = environ['consumer_secret']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
