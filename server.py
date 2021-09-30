0402c111c9318a606d010316a47f5ab332436da0
hammed103 <hammedfree@gmail.com>
2021-09-30 06:42:48 +0100

    df

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
-from os import environ
+import os
 from flask import Flask
-
 app = Flask(__name__)
-app.run(host = '0.0.0', port = environ.get('PORT'))
+
+@user.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
