75b8a3da1cc3a8268c3d719908129eb85de7c340
hammed103 <hammedfree@gmail.com>
2021-09-30 08:07:41 +0100

    n

---
+++ credentials.py
@@ -0,0 +1,6 @@
+#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+
+consumer_key =          'NePesRDyelrnvyZiQsxHFz93z'
+consumer_secret =       'OPY94aJ2YDndSSxhvqJ7C4C23521SkrTAP2dWzCpXwr0I4WgiH'
+access_token =          '1442650978457722881-2vC2X0UIUubRUiJt4IuEwhx3oj3j2A'
+access_token_secret =   'Fw6x7zL7KGFk2JOtXVMYAjLcOebxhZIAkpmu4b0GfQLip'
