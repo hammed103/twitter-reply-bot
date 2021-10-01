16fbf7d46b5beee41780e25cf334ee12c9f7f89e
hammed103 <hammedfree@gmail.com>
2021-10-01 07:03:46 +0100

    l

--- user.py
+++ user.py
@@ -23,7 +23,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #handle without the @
-username = ['license2gain', 'SheyiToBadttt' ] #if sigle user ('xxxyyyy')
+username = ['license2gain' ] #if sigle user ('xxxyyyy')
 
 #keywords in lowercase
 
