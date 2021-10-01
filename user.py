a21107740bca42a38e597f35ad804d3043da576f
hammed103 <hammedfree@gmail.com>
2021-10-01 06:55:37 +0100

    nok

--- user.py
+++ user.py
@@ -5,7 +5,6 @@ from os import environ
 from time import sleep
 import logging
 import random
-from credentials import *
 import schedule
 
 from os import environ
@@ -24,10 +23,10 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #handle without the @
-username = 'license2gain ' #if sigle user ('xxxyyyy')
+username = ['license2gain', 'SheyiToBadttt' ] #if sigle user ('xxxyyyy')
 
 #keywords in lowercase
-keywords = ['#roll', 'kanye','dance'] #if single keyword ['xxxxxyyy']
+
 
 
 
@@ -41,7 +40,7 @@ SLEEP_TIME = 100
 FILE_NAME = 'last_seen.txt'
 
 
-print("Twitter bot that replies {} if any of the keywords {} are found".format(username,keywords ))
+print("Twitter bot that replies users {} ".format(username ))
 
 
 
@@ -51,28 +50,28 @@ result = []
 def reply():
     count = 0
     print ('New session')
-    for tweet in tweepy.Cursor(api.user_timeline,screen_name =username  ,).items():
-        try:
-            if str(tweet.id) not in open('reply_history.txt', 'r').read().split('\n'):
-                if not any(keyword in tweet.text.lower() for keyword in keywords) :
+    for user in username:
+        logger.info("Listening to {}  ".format(user))
+        for tweet in tweepy.Cursor(api.user_timeline,screen_name =user  ,).items(20):
+            try:
+                if str(tweet.id) not in open('reply_history.txt', 'r').read().split('\n'):
+                    sn = tweet.user.screen_name
+                    m = "@{} ".format(sn) + random.choice(open('replies.txt').readlines()).strip("\n")
+                    api.update_status(status=m, in_reply_to_status_id = tweet.id, )
+                    logger.info("Replying  to tweet by {} with {}".format(sn,m))
+                    count  += 1
+                    logger.info("Replied to {} tweet in current session ".format(count))
+                    open('reply_history.txt', 'a').write(str(tweet.id)+'\n')
+                    sleep(SLEEP_TIME)
+                else:
                     continue
-                sn = tweet.user.screen_name
-                m = "@{} ".format(sn) + random.choice(open('replies.txt').readlines()).strip("\n")
-                api.update_status(status=m, in_reply_to_status_id = tweet.id, )
-                logger.info("Replying  to tweet by {} with {}".format(sn,m))
-                count  += 1
-                logger.info("Replied to {} tweet in current session ".format(count))
-                open('reply_history.txt', 'a').write(str(tweet.id)+'\n')
-                sleep(SLEEP_TIME)
-            else:
-                continue
 
-        except tweepy.TweepyException as e:
-            print(e)
+            except tweepy.TweepyException as e:
+                print(e)
 
-        except StopIteration:
-            break
-    logger.info(" {} new tweets by {} replied to ".format(count,username))
+            except StopIteration:
+                break
+            logger.info(" {} new tweets by {} replied to ".format(count,user))
 #Restarts program every x seconds , please make large enough to avoid ban .
 # Depends on how often we expect our user to post new content
 schedule.every(120).seconds.do(reply)
